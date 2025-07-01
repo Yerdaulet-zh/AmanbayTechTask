89d59c00dfc7edd21a46c6f8d99e718c71d1a3ce
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2025-07-01 12:15:17 +0500

    Add func to get only those keys which are needed for encoder block

--- app/utils.py
+++ app/utils.py
@@ -16,3 +16,31 @@ def load_original_whisper_weights(
         map_location=device
     )
     return weights
+
+
+def get_whisper_encoder_keys(
+    encoder_keys: KeysView[str]
+) -> list[str]:
+    """
+    Returns: list[str] 
+    Whisper state dict keys which will be used to extract 
+    from model.pt only required weights for encoder block 
+    (the keys are adjusted for only original whisper layer names). 
+    
+    Note:
+        Some decoder layers are pre-computed at encoder layer, 
+        which can be directly applied in decoder for cross-attention.
+    """
+    
+    encoder_needed_keys = []
+    
+    for key in encoder_keys:
+        if key.startswith('decoder'):
+            key = 'decoder.blocks' + key[len('decoder'):]
+            encoder_needed_keys.append(key)
+            continue
+        
+        key = 'encoder.' + key
+        encoder_needed_keys.append(key)
+    return encoder_needed_keys
+
